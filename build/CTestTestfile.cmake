b6334e6ccaad0215c73a67584bfa30d84c263fbe
laflame <vanya.stalnoy@gmail.com>
2025-12-11 09:59:37 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/vanya/OOP2025LAB3
-# Build directory: /mnt/c/Users/vanya/OOP2025LAB3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(FiguresTests "/mnt/c/Users/vanya/OOP2025LAB3/build/tests")
-set_tests_properties(FiguresTests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/vanya/OOP2025LAB3/CMakeLists.txt;34;add_test;/mnt/c/Users/vanya/OOP2025LAB3/CMakeLists.txt;0;")
